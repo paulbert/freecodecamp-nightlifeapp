a59f990c1f9e3afdb44b2148436914e4262dd4c4
paulbert <paul.r.albert@gmail.com>
2017-03-07 14:18:20 -0500

    Add query parameter for search

--- src/index.js
+++ src/index.js
@@ -5,12 +5,29 @@ import { createStore, applyMiddleware } from 'redux'
 import nightlifeApp from './reducers/nightlifeApp'
 import App from './components/App'
 import thunkMiddleware from 'redux-thunk'
-import { checkLoggedIn } from './actions'
+import { checkLoggedIn, doSearch, changeText } from './actions'
 
 let store = createStore(nightlifeApp,applyMiddleware(thunkMiddleware));
 
+function getQueryVariable(variable) {
+	var query = window.location.search.substring(1);
+	var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		if (decodeURIComponent(pair[0]) == variable) {
+			return decodeURIComponent(pair[1]);
+		}
+	}
+}
+
+let search = getQueryVariable('s');
+
 // Set state based on logged in status
 store.dispatch(checkLoggedIn());
+if(typeof search !== 'undefined') {
+	store.dispatch(doSearch(search));
+	store.dispatch(changeText(search));
+}
 
 render(
 	<Provider store={store}>
